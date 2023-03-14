859a504ce1fdf6c033a2d00d6538f471ba97bbad
Ivan Arias <arias.ivan@gmail.com>
2023-03-14 06:09:38 -0400

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply;
+const num1 = 31;
+const num2 = 2;
+multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod;
+const num3 = 31;
+const num4 = 2;
+mod = 25 % 7;
+
+let max;
+max = Math.max(10, 20); 
